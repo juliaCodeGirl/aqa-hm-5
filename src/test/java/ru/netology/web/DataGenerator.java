238331fe382ed3d133bbe3425335b68e127aad47
juliaCodeGirl <julia.reve19@gmail.com>
2020-11-15 13:19:17 +0300

    new commit

---
+++ src/test/java/ru/netology/web/DataGenerator.java
@@ -0,0 +1,41 @@
+package ru.netology.web;
+
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class DataGenerator {
+
+    private DataGenerator() {
+    }
+
+    public static String getCity() {
+        List list = Arrays.asList(
+                "Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ", "Махачкала", "Магас", "Нальчик", "Элиста",
+                "Черкесск", "Петрозаводск", "Сыктывкар", "Симферополь", "Йошкар-Ола", "Саранск",
+                "Якутск", "Владикавказ", "Казань", "Кызыл", "Ижевск", "Абакан", "Грозный", "Чебоксары",
+                "Барнаул", "Чита", "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь",
+                "Владивосток"
+        );
+        Random item = new Random();
+        String randomElement = (String) list.get(item.nextInt(list.size()));
+        System.out.println(randomElement);
+        return randomElement;
+    }
+
+    public static String getDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String getName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().firstName() + " " + faker.name().lastName();
+    }
+
+    public static String getPhone() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
+    }
+}
